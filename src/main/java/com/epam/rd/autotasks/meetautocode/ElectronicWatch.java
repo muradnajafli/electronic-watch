68f6fdc55d993ca75c0e0cb20d9600d4983a76f7
Murad Najafli <muradnecefli05@gmail.com>
2023-05-19 22:45:33 +0400

    ElectronicWatch

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -7,6 +7,18 @@ public class ElectronicWatch {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
 
+        seconds = Math.max(0, seconds); // this method takes 2 parameters and returns the max value between them
+        // and as I write, if number is negative method returns 0
+
+        int hour = seconds/3600;
+        hour = hour%24;
+
+        int remainder = number%3600;
+        int minute = remainder/60;
+        int second = remainder%60;
+
+        System.out.printf("%02d:%02d:%02d", hour, minute, second);
+
 
     }
 }
